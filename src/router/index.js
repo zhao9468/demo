99e0657631b86c5169e7fb6ac6ce7cdf9169c403
zhao9468 <631841351@qq.com>
2019-03-24 09:48:13 +0800

    登录路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '../components/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      path: '/'
+
+    }, {
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
