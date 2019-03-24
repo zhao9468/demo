5dcb31b6ac614c907f47b3a085dd86ba66a04306
zhao9468 <631841351@qq.com>
2019-03-24 11:19:55 +0800

    头部调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <el-container class="container">
-      <el-header>Header</el-header>
+      <el-header>
+        <el-row>
+          <el-col :span="4">
+            <img src="../assets/logo.png" alt="图片加载失败">
+          </el-col>
+          <el-col :span="19" class="middle">
+            <h2>电商后台管理系统</h2>
+          </el-col>
+          <el-col :span="1">
+            <a href="#" class="logout">退出</a>
+          </el-col>
+        </el-row>
+      </el-header>
       <el-container>
         <el-aside class="aside" width="200px">Aside</el-aside>
         <el-main class="main">Main</el-main>
@@ -16,12 +28,21 @@ export default {};
 
 <style>
 .container {
-  background-color: red;
+  background-color: #b3c0d1;
 }
 .aside {
   background-color: green;
 }
 .main {
   background-color: yellow;
+}
+.middle {
+  line-height: 60px;
+  text-align: center;
+  color: #fff;
+}
+.logout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
